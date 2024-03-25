f8c5e566902be81ab6a35e39501af32c2d1de1c8
pietromalagoli <pietro.malagoli3@studio.unibo.it>
2024-03-25 13:44:40 +0100

    Binomial restaurant exercise added

---
+++ code/binomial_ex_restaurant.R
@@ -0,0 +1,8 @@
+cust <- 4
+p <- 4/15
+x <- 0:4
+ap <- dbinom(x,cust,p)
+barplot(ap, names=x, col='navy', xlab='x', ylab='f(x)', density=40,
+main = sprintf("Binomial distr. Customers=%d, p=%.2f",cust,p),
+cex.lab=1.5, cex.axis=1.25, cex.main=1.25, cex.sub=1.5)
+cat(paste(c("P(2|np) = ", ap[3],'\n')))
