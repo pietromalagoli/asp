dc3d41112b42a07029e67fbbe1afccbe9b133118
pietromalagoli <pietro.malagoli3@studio.unibo.it>
2024-04-29 17:01:28 +0200

    Lab02 point 5 done

--- code/lab02/five.R
+++ code/lab02/five.R
@@ -1,4 +1,19 @@
-n <- (factorial(26) - factorial(16))**2
-d <- factorial(52) - factorial(32)
-p <- n / d
+pCards <- function(x){
+    y <- 26 - x
+
+    n_fav <- (factorial(26) - factorial(y))**2
+    d_fav <- factorial(52) - factorial(2*y)
+    fav <- n_fav / d_fav
+    print(fav)
+
+    n_not <- factorial(y)**2
+    d_not <- factorial(2 * y)
+    not <- n_not / d_not 
+    print(not)
+    
+    p <- fav * not 
+    return(p)
+} 
+
+p <- pCards(10) 
 print(p)
